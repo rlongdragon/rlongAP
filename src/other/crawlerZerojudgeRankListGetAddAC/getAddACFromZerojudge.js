34287dd69385ec46498195a239e4196b3697007d
rlongdragon <95490341+rlongdragon@users.noreply.github.com>
2024-04-20 18:26:47 +0800

    Refactor Zerojudge rank list crawler and add AC statistics

---
+++ src/other/crawlerZerojudgeRankListGetAddAC/getAddACFromZerojudge.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.DB_HOST;
+const DB = "rlongBotAP";
+const COLLECTION = "ZJACHistory";
+const { load } = require('cheerio')
+
+
+async function getACFromZerojudge() {
+  let nameList = process.env.ZEROJUDGE_NAME_LIST
+  let data = []
+
+  for (let p = 1; p <= 3; p++) {
+    const res = await fetch(`https://zerojudge.tw/Ranking?page=${p}&&schoolid=${process.env.ZEROJUDGE_SCHOOL_ID}&tab=tab03`)
+    const text = await res.text()
+    const $ = load(text)
+    let sessionFind = []
+    for (let i = 0; i < nameList.length; i++) {
+      let lname = nameList[i]
+      const nameElement = $(`a[href="Submissions?account=${lname}&status=AC"]`)
+      if (nameElement.length === 0) {
+        continue
+      }
+      data.push({ "name": lname, "AC": nameElement.text() })
+      sessionFind.push(lname)
+    }
+    for (let i = 0; i < sessionFind.length; i++) {
+      nameList.splice(nameList.indexOf(sessionFind[i]), 1)
+    }
+  }
+  for (let i = 0; i < nameList.length; i++) {
+    data.push({ "name": nameList[i], "AC": "0" })
+  }
+  return data
+}
+
+module.exports = async () => {
+
+  await mongoose.connect(`mongodb://${MONGODB_URI}/${DB}`);
+  let lastData = await mongoose.connection.db.collection(COLLECTION).findOne({}, { sort: { createTimeStamp: -1 } });
+  if (!lastData) {
+    console.error("DATA ERROR: No last data")
+  }
+
+  let data = await getACFromZerojudge()
+
+  let insertData = {
+    "info": {
+      "createTimeStamp": Date.now(),
+    },
+    "data": data
+  }
+  mongoose.connection.db.collection(COLLECTION).insertOne(insertData, function (err, res) {
+    if (err) {
+      console.log(`Error: ${err}`);
+      return 1 // error
+    }
+    console.log("1 document inserted");
+    mongoose.connection.close();
+  });
+  
+  // Compare data
+  let compareData = []
+  for(let i = 0; i < data.length; i++) {
+    let name = data[i].name
+    let AC = data[i].AC
+    let lastDataFind = lastData.data.find((e) => e.name === name)
+    if (!lastDataFind) {
+      compareData.push({ "name": name, "AC": AC, "change": AC })
+    } else {
+      compareData.push({ "name": name, "AC": AC, "change": AC - lastDataFind.AC })
+    }
+  }
+
+  return compareData
+}
--- src/other/crawlerZerojudgeRankListGetAddAC/index.js
+++ src/other/crawlerZerojudgeRankListGetAddAC/index.js
@@ -1,8 +1,66 @@
 const { Client } = require('discord.js')
+const getAddACFromZerojudge = require('./getAddACFromZerojudge')
+
+function setWeekWork(clock, week, Client, callback) {
+  const targetHour = clock;
+  const targetDayOfWeek = week;
+
+  const scheduleNextRun = () => {
+    const now = new Date();
+    let targetDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), targetHour, 0, 0);
+
+    if (now.getDay() > targetDayOfWeek || (now.getDay() === targetDayOfWeek && now.getHours() >= targetHour)) {
+      targetDate.setDate(targetDate.getDate() + 7 - now.getDay() + targetDayOfWeek);
+    } else if (now.getDay() < targetDayOfWeek) {
+      targetDate.setDate(targetDate.getDate() + targetDayOfWeek - now.getDay());
+    }
+
+    const delay = targetDate.getTime() - now.getTime();
+    const oneDay = 24 * 60 * 60 * 1000;
+
+    if (delay > oneDay) {
+      setTimeout(() => {
+        scheduleNextRun();
+      }, oneDay);
+    } else {
+      setTimeout(() => {
+        callback(Client);
+        scheduleNextRun();
+      }, delay);
+    }
+  };
+
+  scheduleNextRun();
+}
 
 /**
  * 
  * @param { Client } client 
  */
 module.exports = async function (client) {
+  setWeekWork(17, 7, client,
+    /**
+     * @param {Client} client
+    */
+    async (client) => {
+      const addAC = await getAddACFromZerojudge()
+      // post announcement 
+      let embed = {
+        "type": "rich",
+        "title": `本周解題數統計`,
+        "description": "",
+        "color": 0x21b14c,
+        "fields": []
+      }
+      for (let i = 0; i < addAC.length; i++) {
+        embed.fields.push({
+          "name": addAC[i].name,
+          "value": `AC: ${addAC[i].AC}, 新增: ${addAC[i].change}`
+        })
+      }
+
+      const channel = await client.channels.fetch(process.env.BOT_ANNOUNCEMENT_CHANNEL_ID)
+      channel.send({ embeds: [embed] })
+    }
+  )
 }
